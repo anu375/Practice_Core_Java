6d361f1ac083323241bb9c7c12a9de27d1094606
Anushka Jadhav <anujadhav32004@gmail.com>
2025-12-02 14:19:16 +0530

    first commit

---
+++ src/Practice_Core_Java/Array_Basic.java
@@ -0,0 +1,16 @@
+package Practice_Core_Java;
+
+public class Array_Basic
+{
+
+	public static void main(String[] args) 
+	{
+		int[] arr = {10,20,30,40,50};
+		
+		for(int x : arr)
+		{
+			System.out.println(x);
+		}
+	}
+
+}
